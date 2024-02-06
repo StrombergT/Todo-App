6ad9d70578c5307adcbedec1e3f0b860389183c7
StrombergT <tpr.stromberg@gmail.com>
2024-02-06 23:49:35 +0100

    installed tailwind-css

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import "./TodoApp.css";
 
 interface TodoItem {
   id: string;
@@ -49,31 +48,42 @@ const TodoApp = () => {
 
   return (
     <>
-      <div className="todo-container">
-        <h1 className="todo-header">Todo App!</h1>
+      <div className="max-w-2xl mx-auto  mt-10 bg-gradient-to-r from-green-300 to-blue-300 shadow-md rounded p-8">
+        <h1 className="text-center text-3xl text-blue-700 mb-4 font-bold">
+          Todo App!
+        </h1>
         <input
           type="text"
           value={newTodo}
           onChange={(e) => setNewTodo(e.target.value)}
-          className="todo-input"
+          className="w-4/5 px-4 py-2 mr-4 text-base border border-blue-300 rounded-full focus:outline-none focus:border-blue-500"
         />
-        <button onClick={addTodo} className="add-button">
+        <button
+          onClick={addTodo}
+          className="px-4 py-2 bg-blue-500 text-white rounded-full cursor-pointer text-base transition duration-300 hover:bg-blue-600 mt-4 mx-auto place-items-center focus:outline-none"
+        >
           Add Todo
         </button>
-        <ul className="todo-list">
+        <ul className="list-none">
           {todos.map((todo) => (
-            <li key={todo.id} className="todo-item">
+            <li
+              key={todo.id}
+              className="flex items-center justify-between border-b border-blue-500 py-4 "
+            >
               <input
                 type="checkbox"
                 checked={todo.completed}
                 onChange={() => toggleComplete(todo.id)}
+                className="mr-4"
               />
-              <span className={todo.completed ? "completed" : ""}>
+              <span
+                className={todo.completed ? "line-through text-gray-600" : ""}
+              >
                 {todo.text} - Created at: {todo.createdAt.toLocaleString()}
               </span>
               <button
                 onClick={() => deleteTodo(todo.id)}
-                className="delete-button"
+                className="ml-4 bg-red-500 text-white px-4 py-2 rounded-full cursor-pointer transition duration-300 hover:bg-red-600 focus:outline-none"
               >
                 Delete
               </button>
@@ -81,11 +91,16 @@ const TodoApp = () => {
           ))}
         </ul>
       </div>
-      <div>
-        <h2 className="completed-todo">Completed Todo</h2>
-        <ul className="todo-list">
+      <div className="max-w-2xl mx-auto  mt-10 bg-gradient-to-r from-blue-300 to-green-300 shadow-md rounded p-8">
+        <h2 className="text-2xl text-green-700 mb-4 font-bold text-center">
+          Completed Todo
+        </h2>
+        <ul className="list-none">
           {completedTodos.map((todo) => (
-            <li key={todo.id} className="todo-item-completed">
+            <li
+              key={todo.id}
+              className="line-through text-gray-600 text-center"
+            >
               {todo.text} - Created at: {todo.createdAt.toLocaleString()};
             </li>
           ))}
